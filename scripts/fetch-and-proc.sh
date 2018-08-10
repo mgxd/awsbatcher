ca37bb0e5dc17a10a105f230ffc2093aa7e2c93a
mathiasg <mathiasg@mit.edu>
2018-08-10 14:03:07 -0400

    ENH: support batch job arrays

--- scripts/fetch-and-proc.sh
+++ scripts/fetch-and-proc.sh
@@ -1,41 +1,44 @@
 #!/bin/bash
 
-# install and get a datalad data repository for fmriprep processing
+# install a datalad data repository for fmriprep processing
+# and push results to S3 bucket
 
-set -eu
+set -e
 
 DATASET=$2
-subj=$3
 
-# single use test case
-# $1 http://datasets-tests.datalad.org/abide2/RawData/NYU_2
-# $2 sub-29150
+if [[ -n ${AWS_BATCH_JOB_ARRAY_INDEX} ]]; then
+  # job array
+  subjidx=$(expr ${AWS_BATCH_JOB_ARRAY_INDEX} + 2)
+  SUBJECT=${!subjidx}
+else
+  # single job
+  SUBJECT=$3
+fi
 
-# index list of subjects by batch index
-# subj=${subjs[${AWS_BATCH_JOB_ARRAY_INDEX}]}
+if [[ -z ${DATASET} ]] || [[ -z ${SUBJECT} ]]; then
+  echo "Dataset or subject not properly specified"
+  exit 1
+fi
+
+# set this via CLI
+S3_BUCKET="s3://gablab-fmriprep"
+
+# first, check if subject has already been processed
+aws s3 ls ${S3_BUCKET}/derivatives/fmriprep | grep ${SUBJECT} &> /dev/null
+if [ $? == 0 ] && [[ -z ${S3_OVERWRITE} ]]; then
+  echo "${SUBJECT} has already been processed - set S3_OVERWRITE env to rerun"
+  exit 0
+fi
 
 # make data directory
 mkdir data && pushd data
-
 # first install the remote repository
-datalad install -r $DATASET  # http://datasets-tests.datalad.org/abide2/RawData/${DATASET}
-
-# now fetch only files necessary for fmriprep
-datalad get -r -J 8 ./*/${subj}/{func,anat}/* ./*/${subj}/*/{func,anat}/* ./*/*.json || true
-
+datalad install -r ${DATASET}
+# now fetch only files necessary for single subject fmriprep
+datalad get -r -J 8 ./*/${SUBJECT}/{func,anat}/* ./*/${SUBJECT}/*/{func,anat}/* ./*/*.json || true
 # return to project root
 popd
-
-# remove all symlinks - we may not need to do this since it's all in one container
-
-#for fl in $(find -L . -type f -not -path "./data/*/.*" -regextype posix-egrep -regex ".*\.(json|tsv|gz)$"); do
-#    # we have to be in the same directory as the files (relative symlinks)
-#    pushd $(dirname $fl)
-#    sed -i ';' $(basename $fl)
-#    popd
-#done
-# and remove excess
-#rm -rf data/*/.git data/*/.datalad data/*/.gitattributes
 
 
 # ensure derivatives exists
@@ -48,21 +51,18 @@ if [ ! -d scratch ]; then
     mkdir scratch
 fi
 
-
-echo "cHJpbnRmICJtYXRoaWFzZ0BtaXQuZWR1XG4yNzI1N1xuICpDWG5wZVB3Y2ZLYllcbkZTY3pvTFJBZG9pOHMiID4gL3RtcC8uZnNfbGljZW5zZS50eHQK" | base64 -d | sh
-
 datadir=$(ls $(pwd)/data/* -d)
 
-cmd="fmriprep $datadir derivatives participant \
-    --participant_label $subj \
+# preprocess data
+cmd="fmriprep ${datadir} derivatives participant \
+    --participant_label ${SUBJECT} --cifti-output \
     --nthreads 8 --mem_mb 10000 --output-space template \
     --template-resampling-grid 2mm --ignore slicetiming \
     -w scratch --fs-license-file /tmp/.fs_license.txt"
 
-echo $cmd
-eval $cmd
+echo "Command: ${cmd}"
+eval ${cmd}
 
-# and push results back to s3 bucket
-aws s3 cp derivatives/ s3://gablab-fmriprep/derivatives --recursive
-
+# save outputs to S3 bucket
+aws s3 cp derivatives/ ${S3_BUCKET}/derivatives --recursive --exclude "freesurfer/*"
 exit 0
