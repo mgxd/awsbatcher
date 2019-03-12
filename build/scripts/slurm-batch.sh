f3dd8b367ca4c5fecff11d9e8b6a77ea3b4d5ae5
mathiasg <mathiasg@mit.edu>
2019-03-12 17:39:50 -0400

    doc: polished up sbatch script

--- build/scripts/slurm-batch.sh
+++ build/scripts/slurm-batch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #SBATCH --mem=14GB
-# add other SBATCH arguments here
+#SBATCH --cpus-per-task=8
+#SBATCH --time=2-00:00:00
+#SBATCH --qos=use-everything
 
 if [ $# -eq 0 ]; then
   echo "No arguments passed"
@@ -9,18 +11,19 @@ if [ $# -eq 0 ]; then
 fi
 
 # set any images / variables
-IMG="/storage/gablab001/data/singularity-images/test/aws-fmriprep.sif"
+IMG="/storage/gablab001/data/singularity-images/test/aws/aws-mindboggle.sif"
 
 # set envirnomental variables
 JOBTYPE="fmriprep"
 AWSBATCH_CPUS=8
 AWSBATCH_MEM_MB=14000
 S3_FILE_URL="s3://gitlab-spooky-cache/fetch-and-proc"
-S3_BUCKET=""
+S3_BUCKET="s3://hbn-mindboggle"
 
 # set local working directory
 # should be unique for each subject
 WORKDIR="/om/scratch/`whoami`/awsbatcher/${JOBTYPE}"
 
-CMD="singularity run -B ${WORKDIR}:/working aws-fmriprep.sif $@"
-echo ${CMD}
+CMD="singularity run -B ${WORKDIR}:/working ${IMG} $@"
+printf "Command:\n${CMD}\n"
+${CMD}
